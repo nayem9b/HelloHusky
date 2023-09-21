c7d557d494f127730ecea305f713d441d5877488
Md Razwan Niam <razwanniam1@gmail.com>
2023-09-22 00:40:04 +0600

    data upgradetion from core to auth

--- src/app/modules/academicSemester/academicSemester.event.ts
+++ src/app/modules/academicSemester/academicSemester.event.ts
@@ -1,5 +1,5 @@
 import { RedisClient } from '../../../shared/redis';
-import { EVENT_ACADEMIC_SEMESTER_CREATED } from './academicSemester.constant';
+import { EVENT_ACADEMIC_SEMESTER_CREATED, EVENT_ACADEMIC_SEMESTER_UPDATED } from './academicSemester.constant';
 import { IAcademicSemesterCreatedEvent } from './academicSemester.interface';
 import { AcademicSemesterService } from './academicSemester.service';
 
@@ -8,6 +8,12 @@ const initAcademicSemesterEvents = () => {
     const data: IAcademicSemesterCreatedEvent = JSON.parse(e);
     await AcademicSemesterService.createSemesterFromEvent(data);
     console.log(data);
+  });
+
+  RedisClient.subscribe(EVENT_ACADEMIC_SEMESTER_UPDATED, async (e: string) => {
+    const data: IAcademicSemesterCreatedEvent = JSON.parse(e);
+    await AcademicSemesterService.updateOneIntoDBFromEvent(data);
+    console.log("Updated Data:", data);
   });
 };
 
